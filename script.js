f9b79cd8308929ccd0584397cfe9636a62aa1a91
Jelmer7 <jelmer_beerling@hotmail.com>
2016-01-18 15:40:41 +0100

    vliegendeblokjes

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 window.onload = init;
 var ctx;
-var x = 400;
-var y = 320;
-
-
+var begin = 1200
+var snelheid = 10;
 
 
 function init() {
+	console.log("Init")
 	var canvas = document.querySelector("canvas");
 	ctx = canvas.getContext("2d");
-	drawSpaceShip();
 	animate();
 }
 
-function animate() {
-	drawScene();
-	requestAnimationFrame(animate);
+function drawScreen() {
+	ctx.clearRect(0,0,1200,800);
+	drawObjects();
+	moveShit();
+
 }
 
+function moveShit(){
+	begin -= snelheid;
+}
+
+function drawObjects() {
+	ctx.fillStyle = "red";
+	ctx.fillRect(begin,300,500,50);
+	ctx.fillstyle = "red";
+	ctx.fillRect(begin,750,1200,50);
+}
+
+function animate() {
+	drawScreen();
+	drawObjects();
+	requestAnimationFrame(animate);
+}
